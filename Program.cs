ff68164263584e401267b18b9271da0f9217082d
HelenLev <1711res@gmail.com>
2021-12-12 22:10:16 +0200

    Just Trying

---
+++ Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ///* Задача1. Вывести наибольшее из трех чисел. (Числа вводит пользователь) */
+
+            //int a, b, c;
+            //string str;
+
+
+            //Console.WriteLine("введите первое число");
+            //str = Console.ReadLine();
+            //a = Convert.ToInt32(str);
+
+            //Console.WriteLine("введите второе число");
+            //str = Console.ReadLine();
+            //b = Convert.ToInt32(str);
+
+            //Console.WriteLine("введите третье число");
+            //str = Console.ReadLine();
+            //c = Convert.ToInt32(str);
+
+            //int max = a;
+            //if (b >= max)
+            //{
+            //    max = b;
+            //}
+            //if (c >= max)
+            //{
+            //    max = c;
+            //}
+            //Console.WriteLine("Максимальное число:" + (max));
+            //Console.ReadKey();
+
+            ///* Задача 2. Посчитатть сумму цифр в трехзначном числе.(Число вводит пользователь) */
+
+            //string str;
+            //int sum = 0; //для хранения суммы цифр создаем переменную типа int и присвоим изначально значение = 0                                
+
+            //Console.WriteLine("введите трехзначное число");
+            //str = Console.ReadLine();
+
+            //char[] array = str.ToCharArray();
+            //for (int i = 0; i < array.Length; i++) { sum = sum + (str[i] - '0'); };
+            ///* цикл в котором нужно обойти каждый знак в строке. Создаем переменную i=0. Цикл будет выполняться пока
+            //i меньше длины нашей строки, и каждую итерацию i будет увеличиваться на 1. (В chare хранятся значения из таблицы символов, поэтому будем отнимать 
+            //табличные значения числа 0) каждую итерацию к переменной sum будет прибавляться очередной символ нашей строки от которого будет отнято значение символа 0.
+            //Он в кавычках т.к это не сам 0, а его табличное значение */
+
+            //Console.WriteLine("Сумма чисел:" + (sum));
+            //Console.ReadKey();
+
+            ///* Задача 3. Перевести метры в километры. (Метры вводит пользователь) */
+
+            //Console.WriteLine("Введите кол-во метров которые хотите перевести в км");
+            //double a=Int32.Parse(Console.ReadLine());
+            //double b = a / 1000;
+            //Console.WriteLine("Колличество км=" + b);
+
+            /* Задача 4 По введеному номеру дня неделивывести его название используя оператор множественного выбора switch case */
+
+            
+           
+
+        }
+    }
+}
